4f9ad9ea93b36d6d27f120ca2c05f6f8a4c82075
liuchuan <zhulifei@shulie.io>
2022-04-12 21:57:48 +0800

    格式化!

--- src/test/java/bigmap/BitMap.java
+++ src/test/java/bigmap/BitMap.java
@@ -2,35 +2,35 @@ package bigmap;
 
 public class BitMap {
 
-  private final char[] bitArray;
+    private final char[] bitArray;
 
-  private final int bitArrayLength;
+    private final int bitArrayLength;
 
-  public BitMap(int bitArrayLength) {
-    this.bitArrayLength = bitArrayLength;
-    this.bitArray = new char[bitArrayLength / 16 + 1];
-  }
-
-  public void set(int k) {
-    if (k > bitArrayLength) {
-      return;
+    public BitMap(int bitArrayLength) {
+        this.bitArrayLength = bitArrayLength;
+        this.bitArray = new char[bitArrayLength / 16 + 1];
     }
 
-    int charIndex = k / 16;
-    int bitIndex = k % 16;
-    int i = 1 << bitIndex;
-    bitArray[charIndex] |= i;
-  }
+    public void set(int k) {
+        if (k > bitArrayLength) {
+            return;
+        }
 
-  public boolean get(int k) {
-    if (k > bitArrayLength) {
-      return false;
+        int charIndex = k / 16;
+        int bitIndex = k % 16;
+        int i = 1 << bitIndex;
+        bitArray[charIndex] |= i;
     }
 
-    int charIndex = k / 16;
-    int bitIndex = k % 16;
-    int i = 1 << bitIndex;
-    return (bitArray[charIndex] & i) != 0;
-  }
+    public boolean get(int k) {
+        if (k > bitArrayLength) {
+            return false;
+        }
+
+        int charIndex = k / 16;
+        int bitIndex = k % 16;
+        int i = 1 << bitIndex;
+        return (bitArray[charIndex] & i) != 0;
+    }
 
 }
