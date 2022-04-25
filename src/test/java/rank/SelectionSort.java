22757e7395728696f602c7dae5fbb7580604d96a
liuchuan <zhulifei@shulie.io>
2022-04-25 22:29:34 +0800

    选择排序!

---
+++ src/test/java/rank/SelectionSort.java
@@ -0,0 +1,41 @@
+package rank;
+
+/**
+ * @author liuchuan
+ * @date 2022/4/25 9:58 下午
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 8, 1, 7, 9, 4, 2};
+        sort(arr);
+    }
+
+    private static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+
+            swap(arr, i, minPos);
+        }
+
+        print(arr);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void print(int[] arr) {
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+    }
+
+}
