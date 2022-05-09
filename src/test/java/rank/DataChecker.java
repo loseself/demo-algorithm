c086a1d063f2fa26e61508e3a03f8831062703a4
liuchuan <zhulifei@shulie.io>
2022-05-09 20:15:29 +0800

    优化DataChecker!

--- src/test/java/rank/DataChecker.java
+++ src/test/java/rank/DataChecker.java
@@ -32,20 +32,23 @@ public class DataChecker {
 
     @Test
     public void test() {
-        int[] arr = generateRandomArray();
-        int[] arr2 = new int[arr.length];
-        System.arraycopy(arr, 0, arr2, 0, arr.length);
-
-        Arrays.sort(arr);
-//        InsertionSort.sort(arr2);
-//        ShellSort.knuth(arr2);
-        MergeSort.sort(arr2);
-
         boolean same = true;
-        for (int i = 0; i < arr2.length; i++) {
-            if (arr[i] != arr2[i]) {
-                same = false;
-                break;
+
+        for (int times = 0; times < 500; times++) {
+            int[] arr = generateRandomArray();
+            int[] arr2 = new int[arr.length];
+            System.arraycopy(arr, 0, arr2, 0, arr.length);
+
+            Arrays.sort(arr);
+            //        InsertionSort.sort(arr2);
+            //        ShellSort.knuth(arr2);
+            MergeSort.sort(arr2);
+
+            for (int i = 0; i < arr2.length; i++) {
+                if (arr[i] != arr2[i]) {
+                    same = false;
+                    break;
+                }
             }
         }
 
